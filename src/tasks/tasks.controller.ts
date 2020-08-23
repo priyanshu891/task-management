b18a74a24f0a308da99f34f573334758890a68d1
priyanshu rai <priyanshu.rai891@gmail.com>
2020-08-23 21:23:58 +0530

    Added Task Controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { ApiTags, ApiBody, ApiQuery } from '@nestjs/swagger';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+
+@ApiTags('Task Management')
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService) { }
+
+    @Get()
+    async getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return await this.tasksService.getTasks(filterDto)
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    @ApiBody({ type: CreateTaskDto })
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.tasksService.createTask(createTaskDto)
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.tasksService.getTaskById(id)
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.tasksService.deleteTaskById(id)
+    }
+
+    @Patch('/:id/status')
+    @ApiBody({
+        schema: {
+            example: {
+                status: "string"
+            }
+        },
+        description: 'Status Value Should be OPEN | INPROGRESS | DONE'
+    })
+    updateTaskById(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status)
+    }
+}
