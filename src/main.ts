4b109c699f7ad869a8e1364ba75d42d82cdbcbcd
priyanshu rai <priyanshu.rai891@gmail.com>
2020-08-23 21:20:44 +0530

    Added Support for Swagger

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Task Management')
+    .setDescription('This API is for Task Management Application')
+    .setVersion('1.0')
+    .addTag('Task Management')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
