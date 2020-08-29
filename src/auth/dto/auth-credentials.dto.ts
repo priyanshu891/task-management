d1f6df8083ef18f30913297c1df903d1bf300b2b
priyanshu rai <priyanshu.rai891@gmail.com>
2020-08-29 20:55:27 +0530

    Added AuthCredentialDTO

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator";
+
+export class AuthCredentialDto {
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    username: string;
+
+    @IsString()
+    @MinLength(5)
+    @MaxLength(20)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        { 'message': 'Password too week' }
+    )
+    password: string;
+}
