b3a8d0d3bd0704dadb01f48151e59876c33f3072
priyanshu rai <priyanshu.rai891@gmail.com>
2020-08-29 20:56:45 +0530

    Added Sign Up & Sign In Functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialDto) {
+        return this.authService.signUp(authCredentialsDto)
+    }
+
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialDto) {
+        return this.authService.signIn(authCredentialsDto)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
     constructor(@InjectRepository(UserRepository) private userRepo: UserRepository) { }
+
+    signUp(authCredentialsDto: AuthCredentialDto): Promise<void> {
+        return this.userRepo.signUp(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialDto) {
+        const username = await this.userRepo.validateUserPassword(authCredentialsDto);
+        if (!username) {
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+    }
 }
