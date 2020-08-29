cda7b8e9fd1c1952159df17e10988886590ecf64
priyanshu rai <priyanshu.rai891@gmail.com>
2020-08-29 21:13:24 +0530

    Added Setup for JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: 'topSecret891',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
